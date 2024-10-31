c9877d1b3b34142993fa5fd58d1fc994bae189c9
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2024-10-31 11:36:03 +0100

    Validation update

--- src/main/java/it/gov/pagopa/tpp/dto/TppDTO.java
+++ src/main/java/it/gov/pagopa/tpp/dto/TppDTO.java
@@ -2,6 +2,8 @@ package it.gov.pagopa.tpp.dto;
 
 import it.gov.pagopa.tpp.enums.AuthenticationType;
 import it.gov.pagopa.tpp.model.Contact;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
@@ -12,12 +14,23 @@ import java.time.LocalDateTime;
 @SuperBuilder
 @NoArgsConstructor
 public class TppDTO {
+    @NotBlank(message = "TPP ID must not be blank")
     private String tppId;
+
+    @NotBlank(message = "Entity ID must not be blank")
+    @Pattern(regexp = "^(\\d{11}|[A-Za-z0-9]{1,16})$",
+            message = "Entity ID must be 11 digits or up to 16 alphanumeric characters")
     private String entityId;
     private String idPsp;
     private String businessName;
     private String legalAddress;
+
+    @Pattern(regexp = "^(https?|ftp)://[^ /$.?#].[^ ]*$",
+            message = "Message URL must be a valid URL")
     private String messageUrl;
+
+    @Pattern(regexp = "^(https?|ftp)://[^ /$.?#].[^ ]*$",
+            message = "Authentication URL must be a valid URL")
     private String authenticationUrl;
     private AuthenticationType authenticationType;
     private Contact contact;
--- src/main/java/it/gov/pagopa/tpp/model/Contact.java
+++ src/main/java/it/gov/pagopa/tpp/model/Contact.java
@@ -1,5 +1,7 @@
 package it.gov.pagopa.tpp.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +10,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Contact {
-
     private String name;
+
+    @Pattern(regexp = "^\\d{9,10}$", message = "Number must be between 9 and 10 digits")
     private String number;
+
+    @Email(message = "Email must be valid")
     private String email;
 }
