2ce08aa609235bbf3ee3f3e199b84e16a64699e8
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2025-03-14 11:30:45 +0100

    tpp map implementation

--- src/main/java/it/gov/pagopa/tpp/service/TppMap.java
+++ src/main/java/it/gov/pagopa/tpp/service/TppMap.java
@@ -4,13 +4,14 @@ import it.gov.pagopa.tpp.model.Tpp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 @Slf4j
 public class TppMap {
-    private final Map<String, Tpp> tppCache = new ConcurrentHashMap<>();
+    private final Map<String, Tpp> tppCache = new HashMap<>();
 
     public Tpp getFromMap(String tppId) {
         return tppCache.get(tppId);
