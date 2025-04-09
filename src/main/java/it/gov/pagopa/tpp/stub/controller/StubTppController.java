7cbb8802f8b8bb501598c052e78fbd152c4cc1b9
BaldiVi <vittorio.baldi@emea.nttdata.com>
2025-04-09 10:28:41 +0200

    add test

--- src/main/java/it/gov/pagopa/tpp/stub/controller/StubTppController.java
+++ src/main/java/it/gov/pagopa/tpp/stub/controller/StubTppController.java
@@ -8,14 +8,15 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import reactor.core.publisher.Mono;
 
 
-@RequestMapping("/stub/emd/tpp")
+@RequestMapping("/emd")
 public interface StubTppController {
 
     @PostMapping("/stub/auth")
-    Mono<ResponseEntity<TokenDTO>> auth(@Valid @RequestBody MultiValueMap<String, String> formData);
+    Mono<ResponseEntity<TokenDTO>> auth(@Valid @RequestParam MultiValueMap<String, String> formData);
 
     @PostMapping("/stub/message")
     Mono<ResponseEntity<String>> message(@Valid @RequestBody BaseMessageDTO message);
