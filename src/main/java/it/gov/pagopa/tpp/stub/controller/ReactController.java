25eb11c166fdefd4c1d79aa143abfc359fb6a205
BaldiVi <vittorio.baldi@emea.nttdata.com>
2025-04-09 11:36:11 +0200

    resolve issue

--- src/main/java/it/gov/pagopa/tpp/stub/controller/ReactController.java
+++ src/main/java/it/gov/pagopa/tpp/stub/controller/ReactController.java
@@ -2,11 +2,12 @@ package it.gov.pagopa.tpp.stub.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ReactController {
     @GetMapping("/{path:[^\\.]*}")
-    public String redirect() {
+    public String redirect(@PathVariable String path) {
         return "forward:/index.html";
     }
 }
--- src/main/java/it/gov/pagopa/tpp/stub/controller/StubTppControllerImpl.java
+++ src/main/java/it/gov/pagopa/tpp/stub/controller/StubTppControllerImpl.java
@@ -4,7 +4,6 @@ import it.gov.pagopa.tpp.stub.dto.BaseMessageDTO;
 import it.gov.pagopa.tpp.stub.dto.TokenDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
