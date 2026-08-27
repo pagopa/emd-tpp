af6b4593bf3ec4a5caf9e4eed0908fc9b72e5e7d
fabio.picano <fabio.picano@nttdata.com>
2026-08-27 14:30:28 +0200

    update log

--- src/main/java/it/gov/pagopa/tpp/connector/tpp/TppConnectorAuthImpl.java
+++ src/main/java/it/gov/pagopa/tpp/connector/tpp/TppConnectorAuthImpl.java
@@ -43,7 +43,7 @@ public class TppConnectorAuthImpl implements TppConnectorAuth {
     public Mono<TppConnectionResponseDTO> testConnection(String finalUrl, String contentType, MultiValueMap<String, String> formData) {
         //Used to measure the latency of the request
         long startTime = System.currentTimeMillis();
-        log.info("[TPP-CONNECTOR] Calling external Auth API: {}", finalUrl);
+        log.info("[TPP-CONNECTOR] Calling external Auth API");
 
         return webClient.post()
                 .uri(finalUrl)
